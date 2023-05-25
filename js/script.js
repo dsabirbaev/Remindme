c5f382dae1934bb8e289abcf3810d3b0b77b452d
Davranbek <davranbek_1995@mail.ru>
2023-05-25 21:17:01 +0500

    added project

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+// main slides
+
+$('.users__cards').slick({
+    dots: false,
+    arrows: true,
+    infinite: true,  // бесконечный цикл
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 800,
+    
+});
+
